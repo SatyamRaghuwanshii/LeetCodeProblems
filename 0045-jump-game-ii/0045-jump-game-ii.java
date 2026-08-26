b341d6c40f6a7256a5f4751e18db024e5b213968
SatyamRaghuwanshii <satyamraghuwanshi71@gmail.com>
2026-08-26 19:57:33 +0530

    Time: 2 ms (29.46%), Space: 47.5 MB (21.43%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump = 0;
+        int current = 0;
+        int far = 0;
+        for(int i = 0; i < nums.length-1; i++){
+            far = Math.max(far,i+nums[i]);
+            if(i == current){
+                jump++;
+                current = far;
+            }
+            if(current >= nums.length-1){
+                break;
+            }
+        }
+        return jump;
+    }
+}
