298f9ce4b43ba704a419b2ac728157916a97e745
SatyamRaghuwanshii <satyamraghuwanshi71@gmail.com>
2026-08-27 20:32:08 +0530

    Time: 2 ms (83.58%), Space: 42.2 MB (44.88%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int total = 1;
+        if(num <= 1){
+            return false;
+        }else{
+            for(int i = 2; i*i <= num; i++){
+                if(num%i == 0){
+                    total += i;
+                    if(i != num/i){
+                        total += num/i;
+                    }
+                }
+            }
+        }
+        return total == num;
+    }
+}
